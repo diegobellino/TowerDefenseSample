7853bfe71c4f7dcffb04b1954817fabe4641621e
Diego Bellino <diegobellino>
2022-11-19 19:35:30 +0100

    Add LevelEditor's editing window

--- Assets/Scripts/Editor/LevelEditor/LevelEditorWindow.cs
+++ Assets/Scripts/Editor/LevelEditor/LevelEditorWindow.cs
@@ -1,14 +1,33 @@
-using TowerDefense.Levels;
+using System.Linq;
+using TowerDefense.GameLogic.Runtime.Configs;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using LevelConfig = TowerDefense.Levels.LevelConfig;
 
 namespace TowerDefense.Editor.LevelEditor
 {
     public class LevelEditorWindow : EditorWindow
     {
+        enum ViewType
+        {
+            Main,
+            LevelEditor
+        }
+        
+        private const string LEVEL_EDITOR_SCENE_PATH = "Assets/Scenes/Editor/LevelEditor.unity";
+
+        private ViewType selectedViewType = ViewType.Main;
+        
         private TemplateContainer mainElement;
+        private VisualTreeAsset hordeSpawnerContainer;
+
+        private Scene levelEditorScene;
+        
+        private int spawnersCount;
         
         [MenuItem("TowerDefense/Open Level Editor")]
         public static void ShowExample()
@@ -19,24 +38,63 @@ namespace TowerDefense.Editor.LevelEditor
 
         public void CreateGUI()
         {
-            var root = rootVisualElement;
+            rootVisualElement.Clear();
+            
+            switch (selectedViewType)
+            {
+                case ViewType.Main:
+                    CreateMainView();
+                    break;
+                case ViewType.LevelEditor:
+                    CreateLevelEditorView();
+                    break;
+            }
+            
+            rootVisualElement.Add(mainElement);
+        }
+        
+        public void OnGUI()
+        {
+            if (mainElement == null)
+            {
+                return;
+            }
+            
+            switch (selectedViewType)
+            {
+                case ViewType.Main:
+                    OnMainViewGUI();
+                    break;
+                case ViewType.LevelEditor:
+                    OnLevelEditorGUI();
+                    break;
+            }
+        }
+        
+        #region MAIN_VIEW
+
+        private void CreateMainView()
+        {
+            mainElement?.Clear();
+            
             var visualTree = 
-                AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Editor/LevelEditor/LevelEditorWindow.uxml");
+                AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Editor/LevelEditor/MainWindow.uxml");
             mainElement = visualTree.Instantiate();
+
+            var levelSizePanel = mainElement.Q("level-size-field");
+            levelSizePanel.Add(new Vector2Field("Size (Units)"));
             
             var loadLevelPanel = mainElement.Q("load-level-asset-field");
-
-            var assetField = new ObjectField("Level Asset")
+            loadLevelPanel.Add(new ObjectField("Level Asset")
             {
                 objectType = typeof(LevelConfig)
-            };
+            });
 
-            loadLevelPanel.Add(assetField);
-
-            root.Add(mainElement);
+            mainElement.Q<Button>("create-level-button").clicked += OnCreateLevel;
+            mainElement.Q<Button>("load-level-button").clicked += OnLoadLevel;
         }
 
-        public void OnGUI()
+        private void OnMainViewGUI()
         {
             if (mainElement.Q<RadioButton>("new-level-button").value)
             {
@@ -49,5 +107,91 @@ namespace TowerDefense.Editor.LevelEditor
                 mainElement.Q("load-level-panel").RemoveFromClassList("hide");
             }
         }
+        
+        private void OnCreateLevel()
+        {
+            OpenLevelEditor();
+        }
+        
+        private void OnLoadLevel()
+        {
+            OpenLevelEditor();
+        }
+
+        private void OpenLevelEditor()
+        {
+            levelEditorScene = EditorSceneManager.OpenScene(LEVEL_EDITOR_SCENE_PATH, OpenSceneMode.Additive);
+
+            selectedViewType = ViewType.LevelEditor;
+            CreateGUI();
+        }
+        
+        #endregion
+
+        #region LEVEL_EDITOR_VIEW
+
+        private void CreateLevelEditorView()
+        {
+            spawnersCount = 0;
+
+            mainElement?.Clear();
+
+            var visualTree = 
+                AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Editor/LevelEditor/LevelEditor.uxml");
+            
+            mainElement = visualTree.Instantiate();
+            
+            mainElement.Q("castle-health-field").Add(new IntegerField("Health"));
+            mainElement.Q("castle-position-field").Add(new Vector2Field("Position"));
+            mainElement.Q<Button>("add-spawner-button").clicked += AddSpawnerContainer;
+
+            mainElement.Q<Button>("exit-button").clicked += OnExitLevelEditor;
+        }
+
+        private void OnLevelEditorGUI()
+        {
+            
+        }
+
+        private void OnExitLevelEditor()
+        {
+            EditorSceneManager.CloseScene(levelEditorScene, true);
+            
+            selectedViewType = ViewType.Main;
+            CreateGUI();
+        }
+
+        private void AddSpawnerContainer()
+        {
+            spawnersCount++;
+            
+            hordeSpawnerContainer ??= AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Editor/LevelEditor/HordeSpawnerContainer.uxml");;
+            var newSpawnerContainer = hordeSpawnerContainer.Instantiate();
+
+            var id = $"Spawner {spawnersCount}";
+            newSpawnerContainer.Q<Label>("horde-spawner-id").text = id;
+            newSpawnerContainer.Q("horde-spawner-position-field").Add(new Vector2Field("Position"));
+            newSpawnerContainer.Q("horde-spawner-config-field").Add(new ObjectField("Config")
+            {
+                objectType = typeof(HordeConfig)
+            });
+            newSpawnerContainer.Q<Button>("remove-spawner-button").clicked += () =>
+            {
+                RemoveSpawnerContainer(id);
+            };
+            
+            mainElement.Q("horde-spawners").Add(newSpawnerContainer);
+        }
+
+        private void RemoveSpawnerContainer(string id)
+        {
+            var hordeSpawners = mainElement.Q("horde-spawners");
+            var toRemove = hordeSpawners.Children().First(
+                child => child.Q<Label>("horde-spawner-id").text.Equals(id));
+            hordeSpawners.Remove(toRemove);
+        }
+        
+        #endregion
+        
     }
 }
--- Assets/Scripts/Levels/LevelConfig.cs
+++ Assets/Scripts/Levels/LevelConfig.cs
@@ -6,6 +6,11 @@ namespace TowerDefense.Levels
     [CreateAssetMenu(fileName = "New level config", menuName = "Tower Defense/Levels/New Level")]
     public class LevelConfig : ScriptableObject
     {
+        private Vector2 mapSize;
+        private Vector2[] hordeSpawnerLocations;
+        private Vector2 castleLocation;
+
+        public Vector2 MapSize => mapSize;
 
     }
 }
