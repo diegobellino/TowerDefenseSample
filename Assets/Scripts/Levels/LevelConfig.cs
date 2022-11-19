4194dd284e073cd2546b7671128d95fe52a98612
Diego Bellino <diegobellino>
2022-11-19 16:09:30 +0100

    Add level config base script

---
+++ Assets/Scripts/Levels/LevelConfig.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace TowerDefense.Levels
+{
+    
+    [CreateAssetMenu(fileName = "New level config", menuName = "Tower Defense/Levels/New Level")]
+    public class LevelConfig : ScriptableObject
+    {
+
+    }
+}
